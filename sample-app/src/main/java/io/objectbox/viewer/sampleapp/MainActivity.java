265211a9822937207445604959456cc704afd43c
Marc Reichelt <mcreichelt@gmail.com>
2017-01-27 23:38:42 +0100

    add initial code

---
+++ sample-app/src/main/java/io/objectbox/viewer/sampleapp/MainActivity.java
@@ -0,0 +1,15 @@
+package io.objectbox.viewer.sampleapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import static io.objectbox.viewer.sampleapp.R.layout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_main);
+    }
+}
